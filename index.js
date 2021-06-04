31839aa871f7a867a9903596fe064564eddda7e8
Manav Patnaik <manavpatnaik@hotmail.com>
2021-06-04 17:00:41 +0530

    Initialized app

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const morgan = require("morgan");
+
+// Environment configuration
+dotenv.config();
+
+const app = express();
+
+// Morgan logging
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
+// Middleware
+app.use(express.json());
+
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
