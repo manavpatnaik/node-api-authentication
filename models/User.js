eac8f4374d1302508b251290641bcb38c221b581
Manav Patnaik <manavpatnaik@hotmail.com>
2021-06-06 10:26:03 +0530

    MOdified User schema constraints

--- models/User.js
+++ models/User.js
@@ -1,8 +1,16 @@
 const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
-  email: String,
-  password: String,
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+  },
+  password: {
+    type: String,
+    require: true,
+  },
 });
 
 const User = mongoose.model("User", UserSchema);
